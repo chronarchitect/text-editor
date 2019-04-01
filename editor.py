fe6a33ff1ddf43bcabfdb9c4e14adddf761bf22b
chronarchitect <chronarchitect@MSI-Leopard>
2019-04-01 14:59:37 +0530

    Added Save File Function

--- editor.py
+++ editor.py
@@ -34,8 +34,18 @@ def new_command():
 def exit_command():
     if msgbx.askokcancel(title="Quit?", message="Do you really want to Quit?"):
         root.destroy()
-        
-# Creating Menu
+
+def save_command():
+    file = fdialog.asksaveasfile(mode='w')
+    if file != None:
+        data = text.get('1.0', END+'-1c')
+        file.write(data)
+        file.close()
+def about_command():
+    msgbx.showinfo(title)
+    
+#Creating menu
+
 menu = Menu(root)
 root.config(menu=menu)
 # File
@@ -43,14 +53,13 @@ filemenu = Menu(menu)
 menu.add_cascade(label = "File", menu=filemenu)
 filemenu.add_command(label="New", command=new_command)
 filemenu.add_command(label="Open", command=open_command)
-filemenu.add_command(label="Save", command=dummy)
+filemenu.add_command(label="Save", command=save_command)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=exit_command)
 # Help
 helpmenu = Menu(menu)
 menu.add_cascade(label = "Help", menu=helpmenu)
 helpmenu.add_command(label="About", command=dummy)
-helpmenu.add_command(label="Help", command=dummy)
 
 # button = Button(root, text="Useless")
 # button.pack()
